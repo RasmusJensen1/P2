3fef9e8027c4e9e9ac21025159dfc0acf46533ad
Alb9ert <alberthansen2800@gmail.com>
2024-04-08 14:07:49 +0200

    boom

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var expressLayouts = require("express-ejs-layouts");
+var bodyParser = require("body-parser"); // require body form data???? (til email og pasword)
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,45 @@
 const asyncHandler = require("express-async-handler");
 
-exports.login = asyncHandler(async (req, res, next) => {
+exports.login_get = asyncHandler(async (req, res, next) => {
   res.render("login", {
-    title: "login",
+    title: "Login",
   });
 });
 
-exports.create_account = asyncHandler(async (req, res, next) => {
-  res.render("createaccount", {
-    title: "create_account",
+exports.login_post = asyncHandler(async (req, res, next) => {
+  var data = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+  console.log(data);
+
+  res.redirect("my-budgets");
+});
+
+// // Tjek for at se om det virker
+// console.log("Username:", email);
+// console.log("Password:", password);
+// // Sender til bruger email og kode virker
+// res.send("Form submitted successfully!");
+
+exports.signup_get = asyncHandler(async (req, res, next) => {
+  res.render("login", {
+    title: "Sign up",
   });
+});
+
+exports.signup_post = asyncHandler(async (req, res, next) => {
+  var newData = {
+    newEmail: req.body.newEmail,
+    newPassword: req.body.newPassword,
+    repeatPassword: req.body.repeatPassword,
+  };
+
+  console.log(newData);
+
+  if (newData.newPassword === newData.repeatPassword) {
+    res.redirect("login");
+  } else {
+    res.redirect("sign-up");
+  }
 });
