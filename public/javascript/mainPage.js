8d285619d74835ab1639e16504bf1f5d71f86c84
RasmusJensen1 <rasendemus5@gmail.com>
2024-04-17 10:13:06 +0200

    css igang

--- public/javascript/mainPage.js
+++ public/javascript/mainPage.js
@@ -5,9 +5,12 @@ const expense = document.createElement('div');
 const circle = document.createElement('div');
 const input = document.createElement('input');
 
+
 circle.textContent = "house";
 expense.appendChild(circle);
 input.setAttribute('type', 'range');
+input.setAttribute('class', 'slider');
+input.setAttribute('id', 'my-range');
 
 expense.appendChild(input)
 expense.setAttribute('class','expense');
