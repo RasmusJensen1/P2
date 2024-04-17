da15beb4deeaa5f50f1c51eb2b7537bdfeab17c9
Alb9ert <alberthansen2800@gmail.com>
2024-04-17 12:37:23 +0200

    save¨

--- public/javascript/mainPage.js
+++ public/javascript/mainPage.js
@@ -1,18 +1,45 @@
-console.log("mainPage");
+const root = document.getElementById("slider-container"); //or target div
 
-const root = document.getElementById('slider-container') //or target div
-const expense = document.createElement('div');
-const circle = document.createElement('div');
-const input = document.createElement('input');
+const expense = document.createElement("div");
 
+const circle = document.createElement("div");
 
-circle.textContent = "house";
+const input_container = document.createElement("div");
+const input = document.createElement("input");
+const value = document.createElement("div");
+
+circle.textContent = "House";
 expense.appendChild(circle);
-input.setAttribute('type', 'range');
-input.setAttribute('class', 'slider');
-input.setAttribute('id', 'my-range');
 
-expense.appendChild(input)
-expense.setAttribute('class','expense');
+expense.appendChild(input_container);
+input_container.appendChild(input);
+input_container.appendChild(value);
+
+input.setAttribute("type", "range");
+input.setAttribute("class", "slider");
+input.setAttribute("id", "my-range");
+input.setAttribute("min", "0");
+input.setAttribute("max", "100");
+input.setAttribute("value", "50");
+
+value.setAttribute("class", "show-value");
+
+circle.setAttribute("class", "circle");
+
+expense.setAttribute("class", "expense");
+
+input_container.setAttribute("class", "input-container");
 
 root.appendChild(expense);
+
+const rangeInput = document.querySelector('input[type="range"]');
+const rangeText = document.querySelector(".show-value");
+
+value.textContent = 50 + " DKK";
+
+rangeInput.addEventListener("change", (e) => {
+  let newVal = e.target.value;
+  let negNewVal = -1 * newVal;
+
+  value.textContent = newVal + " DKK";
+});
