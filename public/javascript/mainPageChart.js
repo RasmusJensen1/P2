a753374d8095813e709489046e12c920e68c4509
Emil Lykke <emillykkeg@gmail.com>
2024-04-17 12:31:08 +0200

    working with data lables

--- public/javascript/mainPageChart.js
+++ public/javascript/mainPageChart.js
@@ -7,20 +7,34 @@ labels.push('Surplus')
 const totalIncome = selectedBudget.totalIncome
 
 const surplusPercentage = 1 - selectedBudget.expenses.reduce((prev, next) => prev+ next.part, 0)
-const surplus = totalIncome * surplusPercentage
+const surplus = surplusPercentage * 100
 
-const dataPoints = selectedBudget.expenses.map(expense => expense.part*totalIncome)
+const dataPoints = selectedBudget.expenses.map(expense => expense.part*100)
 dataPoints.push(surplus)
 
-
-new Chart(chart, {
+const config ={
     type: 'pie',
     data: {
-      labels,
+      labels: labels,
       datasets: [{
-        label: 'kr.',
-        data: dataPoints,
+        label: '%',
+        data: dataPoints.map(part => Math.round(part)),
         borderWidth: 1
       }]
     },
-  });
+    options: {
+        plugins: {
+            datalabels: {
+                formatter: (value) => value/100*totalIncome + ' kr.',
+                anchor: 'end',
+                clamp: true,
+                align: 'start',
+                color: '#000',
+            }
+        }
+    },
+    plugins: [ChartDataLabels]
+} 
+
+
+new Chart(chart, config);
