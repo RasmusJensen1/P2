1084ee8ff056d81b8ae21269c71801faa947bd08
Alb9ert <alberthansen2800@gmail.com>
2024-04-17 13:44:11 +0200

    for now

--- public/javascript/mainPage.js
+++ public/javascript/mainPage.js
@@ -1,42 +1,49 @@
-for (let i = 0; i < 10; i++) {
-  const root = document.getElementById("slider-container"); //or target div
+const chosenBudget = budget;
 
-  const expense = document.createElement("div");
+console.log(chosenBudget.expenses);
 
+const root = document.getElementById("slider-container");
+
+function calculate_amount(part) {
+  return budget.totalIncome * part;
+}
+
+chosenBudget.expenses.forEach((expense) => {
+  //   Creates each expense changer
+  const expense_div = document.createElement("div");
   const circle = document.createElement("div");
-
   const input_container = document.createElement("div");
   const input = document.createElement("input");
   const value = document.createElement("div");
 
-  circle.textContent = "House";
-  expense.appendChild(circle);
+  circle.textContent = expense.expenseName;
+  expense_div.appendChild(circle);
 
-  expense.appendChild(input_container);
+  expense_div.appendChild(input_container);
   input_container.appendChild(input);
   input_container.appendChild(value);
 
   input.setAttribute("type", "range");
   input.setAttribute("class", "slider");
-  input.setAttribute("id", "my-range");
+  input.setAttribute("id", expense.name);
   input.setAttribute("min", "0");
-  input.setAttribute("max", "100");
-  input.setAttribute("value", "50");
+  input.setAttribute("max", budget.totalIncome);
+  input.setAttribute("value", calculate_amount(expense.part));
 
   value.setAttribute("class", "show-value");
 
   circle.setAttribute("class", "circle");
 
-  expense.setAttribute("class", "expense");
+  expense_div.setAttribute("class", "expense_div");
 
   input_container.setAttribute("class", "input-container");
 
-  root.appendChild(expense);
+  root.appendChild(expense_div);
 
   const rangeInput = document.querySelector('input[type="range"]');
   const rangeText = document.querySelector(".show-value");
 
-  value.textContent = 50 + " DKK";
+  value.textContent = calculate_amount(expense.part) + " DKK";
 
   rangeInput.addEventListener("change", (e) => {
     let newVal = e.target.value;
@@ -44,4 +51,13 @@ for (let i = 0; i < 10; i++) {
 
     value.textContent = newVal + " DKK";
   });
-}
+
+  rangeInput.addEventListener("input", function () {
+    const x =
+      ((rangeInput.value - rangeInput.min) /
+        (rangeInput.max - rangeInput.min)) *
+      100;
+    const color = `linear-gradient(90deg, #3c67f4 ${x}%, #ffffff ${x}%)`;
+    rangeInput.style.background = color;
+  });
+});
