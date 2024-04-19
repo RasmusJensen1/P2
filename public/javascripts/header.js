09f5f13e982d1008470a3757af5292b253bcae42
Alb9ert <alberthansen2800@gmail.com>
2024-04-19 10:21:34 +0200

    farve

---
+++ public/javascripts/header.js
@@ -0,0 +1,8 @@
+var currentPath = window.location.pathname;
+
+if (currentPath == "/info") {
+  let info = document.getElementById("info");
+  info.style.fontWeight = "bold";
+}
+
+// nåede vi ikke at finde ud af
---
+++ public/javascripts/passwordMatch.js
@@ -0,0 +1,12 @@
+// document
+//   .getElementById("create-account-btn")
+//   .addEventListener("click", function () {
+//     const newUsername = document.getElementById("newUsername").value;
+//     const newPassword = document.getElementById("newPassword").value;
+//     const repeatPassword = document.getElementById("repeatPassword").value;
+
+//     console.log("New Username:", newUsername);
+//     console.log("New Password:", newPassword);
+//     console.log("Repeat Password:", repeatPassword);
+
+//   });
