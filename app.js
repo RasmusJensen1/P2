c6b81b716385baaacfc3ac8aa6269c5aea02806a
Alb9ert <alberthansen2800@gmail.com>
2024-04-23 15:18:30 +0200

    true strict query

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-mongoose.set("strictQuery", false);
+mongoose.set("strictQuery", true);
 
 const mongoDB = process.env.MONGODB_URL;
 
