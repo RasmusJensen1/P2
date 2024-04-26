4f416689bf8c7b02320436b0753c026b761b577c
Emil Lykke <emillykkeg@gmail.com>
2024-04-26 13:20:52 +0200

    pr fix

--- controllers/budgetinstanceController.test.js
+++ controllers/budgetinstanceController.test.js
@@ -4,13 +4,13 @@ const request = require("supertest");
 describe("Testing GET request for /budgetinstance/:id", () => {
 
    // Test the "/budgetinstance/:id" GET route where the budget is found
-    test("Test the /budgetinstance/:id GET, Should respond with status code 200", async () => {
+    test("Should respond with status code 200", async () => {
       const response = await request(app).get("/budgetinstance/661e7db82f305c0bf9fcb291");
       expect(response.statusCode).toBe(200);
     }); 
 
     // Test the "/budgetinstance/:id" GET route where the budget is not found
-    test("Test the /budgetinstance/:id GET, Should respond with status code 404", async () => {
+    test("Should respond with status code 404", async () => {
      // 661e7db82f305c0bf9fcb290 is not a budget id in our database
       const response = await request(app).get("/budgetinstance/661e7db82f305c0bf9fcb290");
       expect(response.statusCode).toBe(400);
@@ -18,7 +18,7 @@ describe("Testing GET request for /budgetinstance/:id", () => {
     });
 
     // Test the "/budgetinstance/:id" GET route where there is an error
-    test("Test the /budgetinstance/:id GET, Should respond with status code 404", async () => {
+    test("Should respond with status code 404", async () => {
       // 661e7db82f305c0bf9fcb290 is not a budget id in our database
       const response = await request(app).get("/budgetinstance");
       expect(response.statusCode).toBe(404);
