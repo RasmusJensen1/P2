cd0610449bad4b51fc635fe16b75cd5285a28e89
Emil Lykke <emillykkeg@gmail.com>
2024-04-27 05:24:42 +0200

    database check for budgetinstance

--- controllers/budgetinstanceController.test.js
+++ controllers/budgetinstanceController.test.js
@@ -14,17 +14,22 @@ describe("Testing GET request for /budgetinstance/:id", () => {
     })
 
    // Test the "/budgetinstance/:id" GET route where the budget IS found	
-    test("Should respond with status code 200", async () => {	
+    test("Should respond with status code 200, when budget IS found", async () => {	
         const response = await request(app).get(`/budgetinstance/${budget._id.toString()}`);	
         expect(response.statusCode).toBe(200);	
     }); 	
 
     // // Test the "/budgetinstance/:id" GET route where the budget is not found	
     test("Should respond with status code 400", async () => {	
-     // 661e7db82f305c0bf9fcb290 is not a budget id in our database	
-      const response = await request(app).get("/budgetinstance/661e7db82f305c0bf9fcb290");	
-      expect(response.statusCode).toBe(400);	
-      expect(response.headers.location).toBe("/my-budgets");
+        // 661e7db82f305c0bf9fcb290 is not a budget id in our database	
+        const id = "661e7db82f305c0bf9fcb290"
+        const response = await request(app).get(`/budgetinstance/${id}`);	
+
+        expect(response.statusCode).toBe(400);	
+        expect(response.headers.location).toBe("/my-budgets");
+
+        const budget = await Budget.findById(id).exec();
+        expect(budget).toBeNull();
     });	
 
     // Test the "/budgetinstance/:id" GET route where there is an error	
